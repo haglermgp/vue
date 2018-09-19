79c6fa7b3a1c83a60b92afda18f3171d4ba0aa93
hagler.mijail <hagler.mijail@gmail.com>
2018-09-18 19:56:01 -0500

    add bind value fiels

--- basic-web-app/src/App.vue
+++ basic-web-app/src/App.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app" >
-    <header>{{ title }}</header>
+    <input type="checkbox" id='checkbox' v-model="checked" />
+    <label for="checkbox">
+      This box is <b> {{ checked ? 'checked' : 'uncheked'}} </b>
+    </label>
     <hr>
-    <form v-on:submit.prevent="addDinos">
-      <button >Add Dinosaur</button>
-      <input type="number" v-model='amount'>
-    </form>
-    <p>
-      You have {{ total }} Dinosours!
-    </p>
+    <template v-for='dino in dinos' >
+      <label>
+        <input type="radio" v-bind:value="dino" v-model="chosenDino" >
+        {{ dino }}
+      </label>
+    </template>
+    <br>
+    <span>Favorite: {{ chosenDino }}</span>
+    <hr>
+
+    <select v-model="selectedPeriods" multiple>
+      <option v-for="period in periods" v-bind:value="period.value">
+        {{ period.name }} - {{ period.value }}
+      </option>
+    </select>
+    <br>
+    <span>Selected Period Value: {{ selectedPeriods }}</span>
+
+    <hr>
+    <input type="text" v-model="single" />
+    <p style="white-space: pre;">{{ single }}</p>
+    <hr>
+    <textarea v-model="message" placeholder="add multiple lines" ></textarea>
+    <p style="white-space: pre;" >{{ message }}</p>
   </div>
 </template>
 
@@ -17,14 +37,19 @@ export default {
   name: 'app',
   data () {
     return {
-      title: 'Dinosours',
-      total: 0,
-      amount: null
-    }
-  },
-  methods: {
-    addDinos: function() {
-      this.total += Number(this.amount);
+      checked: false,
+      chosenDino: '',
+      selectedPeriods: [],
+      single: '',
+      message: '',
+      dinos: [
+        "Triceratops", "Velociraptor", "Tyrannosaurus"
+      ],
+      periods: [
+        { name: 'Triassic', value: 1 },
+        { name: 'Jurasic', value: 2 },
+        { name: 'Cuaternary', value: 3 },
+      ]
     }
   }
 }
